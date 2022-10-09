b6003550038751c98233905b22dde9bbe4e6ddb8
garvkapoor7 <lucifergarv123@gmail.com>
2022-10-09 16:14:07 +0530

    aths2

--- add.py
+++ add.py
@@ -1,7 +1,3 @@
-#create two variables and assign values to them
-
-# create a third variable(sum) and add x add 
-#print the sum 
-x=5
-y=3
-z=print("the answer is:", x+y)
+x= input("enter the first no:")
+y=input("enter the ssecond no :")
+print(x+y)
